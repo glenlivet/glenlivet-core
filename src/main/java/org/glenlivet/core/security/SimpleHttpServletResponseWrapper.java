d6661d78e755a1d163a7ef06dec248ab0026aa02
GZhang <glenlivet.zhang@cnsdc01.pwc.com>
2015-08-18 17:46:09 +0800

    test SimpleHttpServletResponseWrapper

--- src/main/java/org/glenlivet/core/security/SimpleHttpServletResponseWrapper.java
+++ src/main/java/org/glenlivet/core/security/SimpleHttpServletResponseWrapper.java
@@ -3,6 +3,12 @@ package org.glenlivet.core.security;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
+/**
+ * NOT USED.
+ * 
+ * @author gzhang061
+ *
+ */
 public class SimpleHttpServletResponseWrapper extends HttpServletResponseWrapper{
 
 	public SimpleHttpServletResponseWrapper(HttpServletResponse response) {
