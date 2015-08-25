ea8359d803fcbac4b80ff028eb8d88d0ebd790fa
GZhang <glenlivet.zhang@cnsdc01.pwc.com>
2015-08-25 18:04:19 +0800

    annotation authenticated added.

---
+++ src/main/java/org/glenlivet/core/security/Authenticated.java
@@ -0,0 +1,14 @@
+package org.glenlivet.core.security;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface Authenticated {
+	
+	String[] hasRole() default "";
+
+}
