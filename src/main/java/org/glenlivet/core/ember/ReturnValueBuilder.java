8aa2211cae37fcefc0a52ebeab5bd504bd09fc0e
Zhang, Glenlivet <kikyou1986@hotmail.com>
2015-11-22 22:03:51 +0800

    ember return value builder added.
    ember return value builder added.

---
+++ src/main/java/org/glenlivet/core/ember/ReturnValueBuilder.java
@@ -0,0 +1,20 @@
+package org.glenlivet.core.ember;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReturnValueBuilder {
+	
+	public static Map<String, Object> buildFindAll(String[] keys, @SuppressWarnings("rawtypes") List... lists){
+		if(lists.length != keys.length){
+			throw new IllegalArgumentException("The numbers of keys and values are not match.");
+		}
+		Map<String, Object> retVal = new HashMap<String, Object>();
+		for(int i = 0; i < keys.length; i++){
+			retVal.put(keys[i], lists[i]);
+		}
+		return retVal;
+	}
+
+}
