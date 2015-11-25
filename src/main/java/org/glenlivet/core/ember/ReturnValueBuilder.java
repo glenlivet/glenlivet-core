f3491b100434fddefa8bc45e67d1ce38fdd12ab5
Zhang, Glenlivet <kikyou1986@hotmail.com>
2015-11-26 01:34:37 +0800

    retval builder updated.

--- src/main/java/org/glenlivet/core/ember/ReturnValueBuilder.java
+++ src/main/java/org/glenlivet/core/ember/ReturnValueBuilder.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.glenlivet.core.BaseDomain;
+
 public class ReturnValueBuilder {
 	
 	public static <T> Map<String, Object> buildSingleFind(String key, T obj){
@@ -20,6 +22,14 @@ public class ReturnValueBuilder {
 		for(int i = 0; i < keys.length; i++){
 			retVal.put(keys[i], lists[i]);
 		}
+		return retVal;
+	}
+
+	public static Map<String, Object> buildReturnId(String key, BaseDomain domain) {
+		Map<String, Object> retVal = new HashMap<String, Object>();
+		Map<String, String> idMap = new HashMap<String, String>();
+		idMap.put("id", domain.getId());
+		retVal.put(key, idMap);
 		return retVal;
 	}
 
